8006c88b9099584b50c91f944379dd76c616ae23
DanielSz <danielszaboa@gmail.com>
2023-07-26 21:48:52 +0200

    Added the array creator

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
-from tkinter import ttk
-from tkinter import *
+#import pygame
+
 
 
 def bubble_soring(array):
@@ -26,28 +26,42 @@ def binary_sorting(array):
 
 def update_screen(array):
     not_sorted = True
-    while (not_sorted):
+    while not_sorted:
         pass
     pass
 
 
-def create_array(size, min, max):
+def create_array(size, min, max, unique_numbers):
     random_array = []
-    for i in range(size):
-        random_array.append(random.randint(min, max))
+    if unique_numbers:
+        if max-min >= size:
+            filled = False
+            not_used_numbers = [True]*(max-min+1)
+            while not(filled):
+                new_random_number = random.randint(min, max)
+                #Checks if the new random number that is created already in use
+                if not_used_numbers[new_random_number-min]:
+                    random_array.append(new_random_number)
+                    print(random_array)
+                    not_used_numbers[new_random_number - min] = False
+                    if len(random_array) == size:
+                        filled = True
+
+        else:
+            print("Error")
+            print("There are not enough nuber to fill this size creating with duplicates")
+            for i in range(size):
+                random_array.append(random.randint(min, max))
+    else:
+        print("here")
+        for i in range(size):
+            random_array.append(random.randint(min, max))
     return random_array
 
 
+def run_sorting():
+    pass
+
+
 if __name__ == "__main__":
-    root = Tk()
-    root.geometry("900x700")
-    root.title("Sorting Visualizer")
-    rand_array = create_array(5, 1, 1)
-    print()
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-    #ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
-    #ttk.Button(frm, text="Quit", command=root.destroy).grid(column=1, row=0)
-    root.mainloop()
-    #sorable_array = create_array()
-    #bubble_soring(sorable_array)
+    print(create_array(15,5,15,True))
